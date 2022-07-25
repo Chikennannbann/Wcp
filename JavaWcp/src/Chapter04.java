e074131ff596d0d8a5d656767452823e603bef6a
MisakiKawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-07-25 17:49:41 +0900

    [add] 2回目Chapter6

--- JavaWcp/src/Chapter04.java
+++ JavaWcp/src/Chapter04.java
@@ -8,5 +8,16 @@ public class Chapter04 {
 		str = "panda";
 		System.out.println(str);
 		System.out.println(LUCKY_NUMBER);
+		
+		
+		// Chapter04 2回目
+		String string = "MISAKI";
+		System.out.println(string);
+				
+		string = "PANDA";
+		System.out.println(string);
+				
+		final int LUCKY = 777;
+		System.out.println(LUCKY);
 	}
 }
