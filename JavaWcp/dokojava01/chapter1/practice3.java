9bce74bd2bc6f5262705748d27c7573833e7e8b5
MisakiKawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-08-16 19:05:30 +0900

    [start] dokojava chapter1,2

---
+++ JavaWcp/dokojava01/chapter1/practice3.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+public class practice3 {
+	public static void main(String[] args) {
+		boolean result = true;
+		char moji = '駆';
+		double pi = 3.14;
+		long number = 3124159265853979L;
+		String message = "みなとの攻撃！的に15ポイントのダメージを与えた";
+		
+		System.out.println(result);
+		System.out.println(moji);
+		System.out.println(pi);
+		System.out.println(number);
+		System.out.println(message);
+		
+	}
+
+}
