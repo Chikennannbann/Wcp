f303d18a80ee35905b8ef1e41adb6f3b53e0e98e
MisakiKawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-08-27 22:54:40 +0900

    [add] dokojava chapter9,10

--- JavaWcp/dokojava01/Main.java
+++ JavaWcp/dokojava01/Main.java
@@ -1,28 +1,41 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
+		Sword s = new Sword();
+		s.name = "炎の剣";
+		s.damage = 10;
+//		Hero h = new Hero();
+//		h.hp = 100;
+		Hero h = new Hero("ミナト");
+		h.sword = s;
 		
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
+		Wizard w = new Wizard();
+		w.name = "アサカ";
+		w.hp = 30;
 		
-		Matango m2 = new Matango();
-		m2.hp = 48;
-		m2.suffix ='B';
+		Matango m = new Matango('A');
+		
+		PoisonMatango pm = new PoisonMatango('C');
+		
+		SuperHero sh = new SuperHero();
+		sh.run();
 		
 		System.out.println("勇者" + h.name + "を生み出しました！");
+		System.out.println("現在の武器は" + h.sword.name);
 		
 //		h.sit(5);
 //		h.slip();
 //		h.sit(25);
 //		h.run();
-		
+		h.attack(m);
 		h.slip();
-		m1.run();
-		m2.run();
+		w.heal(h);
+		m.run();
+		
+		pm.attack(h);
+		
 		h.run();
+		
+		
 	}
 }
