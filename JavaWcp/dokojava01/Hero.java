b9e428281e6818ced5562a961b2a7aa4597269f7
MisakiKawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-09-02 23:27:32 +0900

    [add] dokojava chapter13

--- JavaWcp/dokojava01/Hero.java
+++ JavaWcp/dokojava01/Hero.java
@@ -1,6 +1,6 @@
 
 public class Hero {
-	String name;
+	private String name;
 	int hp;
 	Sword sword;
 	
@@ -10,7 +10,8 @@ public class Hero {
 		System.out.println("敵に5ポイントのダメージを与えた！");
 	}
 	
-	public void sleep() {
+//	アクセス修飾飾を外すとpackage privateを指定したと見なされ同じパッケージに属するクラスからの呼び出しのみ可能
+	void sleep() {
 		this.hp = 100;
 		System.out.println(this.name + "は、眠って回復した！");
 	}
@@ -32,6 +33,11 @@ public class Hero {
 		System.out.println("GAMEOVER");
 		System.out.println("最終HPは" + this.hp + "でした。");
 	}
+//	アクセス修飾飾がprivateでも同じクラス内のメソッドからはthisを使って呼び出し可能
+	private void die() {
+		System.out.println(this.name + "は死んでしまった！");
+		System.out.println("GAME OVERです。");
+	}
 	
 	public Hero(String name) {
 		this.hp = 100;
@@ -41,6 +47,16 @@ public class Hero {
 	public Hero() {
 		this("ダミー");
 //		このthisは同一クラスの別のコンストラクターを呼び出すためのもの
+	}
+	
+//	getter(read only)
+	public String getName() {
+		return this.name;
+	}
+	
+//	setter(write only)
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
---
+++ JavaWcp/dokojava01/chapter13/Main.java
@@ -0,0 +1,9 @@
+package chapter13;
+
+public class Main {
+	public static void main(String[] args) {
+		Code13 h = new Code13();
+		h.setName("ああああああああああああ");
+	}
+
+}
