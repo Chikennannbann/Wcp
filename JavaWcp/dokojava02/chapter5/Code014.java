baafc9fc712ba1b5c79ee9e1763bed6d38fa33c6
MisakiKawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-09-21 23:50:16 +0900

    [add] dokojava02 chapter5

---
+++ JavaWcp/dokojava02/chapter5/Code014.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+public class Code014 {
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return newArray;
+	}
+	
+	public static void main(String[] aegs) {
+		int[] array = makeArray(3);
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
+
+}
